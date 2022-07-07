004c38213aced1c61d7c16d6e0bb1ce248bab8c5
unknown <muhammadsaadz100@gmail.com>
2022-07-07 16:17:22 +0500

    Added search screen

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,105 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gram/utils/colors.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  final TextEditingController searchController = TextEditingController();
+  bool showUsers = false;
+  @override
+  void dispose() {
+    super.dispose();
+    searchController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        title: TextFormField(
+            onChanged: (value) {
+              if (value.isNotEmpty) {
+                setState(() {
+                  showUsers = true;
+                });
+              } else {
+                setState(() {
+                  showUsers = false;
+                });
+              }
+            },
+            controller: searchController,
+            decoration: const InputDecoration(label: Text("Search for users"))),
+      ),
+      body: showUsers
+          ? FutureBuilder(
+              future: FirebaseFirestore.instance
+                  .collection('users')
+                  .where('username',
+                      isGreaterThanOrEqualTo: searchController.text)
+                  .get(),
+              builder: (context,
+                  AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+                if (!snapshot.hasData ||
+                    snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return ListView.builder(
+                    itemCount: (snapshot.data! as dynamic).docs.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: NetworkImage(
+                              (snapshot.data! as dynamic).docs[index]
+                                  ['photoUrl']),
+                        ),
+                        title: Text((snapshot.data! as dynamic).docs[index]
+                            ['username']),
+                      );
+                    });
+              },
+            )
+          : FutureBuilder(
+              future: FirebaseFirestore.instance
+                  .collection('posts')
+                  .orderBy('likes', descending: true)
+                  .get(),
+              builder: ((context, snapshot) {
+                if (!snapshot.hasData ||
+                    snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                return SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: StaggeredGrid.count(
+                    crossAxisCount: 4,
+                    mainAxisSpacing: 4,
+                    crossAxisSpacing: 4,
+                    children: [
+                      for (int i = 0;
+                          i < (snapshot.data as dynamic).docs.length;
+                          i++)
+                        StaggeredGridTile.count(
+                            crossAxisCellCount: 2,
+                            mainAxisCellCount: 2,
+                            child: Image(
+                                image: NetworkImage(
+                              (snapshot.data! as dynamic).docs[i]['postUrl'],
+                            )))
+                    ],
+                  ),
+                );
+              })),
+    );
+  }
+}
